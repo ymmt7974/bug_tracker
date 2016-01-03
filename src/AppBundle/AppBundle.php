69a0067fb21162a966c9d08abdcd17f49ee9ffba
tsubo <ktsubota@gmail.com>
2016-01-03 22:20:16 +0900

    sqliteで外部キー制約を有効にするようPRAGMA文の発行を追加

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -6,4 +6,12 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
 {
+    public function boot()
+    {
+        // SQLite foreign key support
+        $conn = $this->container->get('database_connection');
+        if ($conn->getParams()['driver'] == 'pdo_sqlite') {
+            $conn->exec('PRAGMA foreign_keys=1');
+        }
+    }
 }
